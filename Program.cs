5c4c5efa03812858fd3d3483a42ff96c98e50f2a
Kubaikin Dmitry <dimian150@gmail.com>
2020-09-26 18:33:16 +0300

    end

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using lib_int;
 
 namespace library
 {
@@ -10,6 +11,42 @@ namespace library
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+
+            int[] arr = new int[20];
+
+            Console.WriteLine("Рандомный массив");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(100);
+                Console.Write(arr[i] + "  ");
+            }
+            Console.WriteLine();
+
+            Console.Write("\nMin -> ");
+            Console.WriteLine(library_int.Min(arr));
+            Console.WriteLine();
+
+            Console.Write("Max -> ");
+            Console.WriteLine(library_int.Max(arr));
+            Console.WriteLine();
+
+            Console.Write("Среднее -> ");
+            Console.WriteLine(library_int.Summ(arr));
+            Console.WriteLine();
+
+            Console.Write("Перевернутое -> ");
+            library_int.Revers(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]+"  ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine();
+            string str = library_int.Conver(arr);
+            Console.WriteLine(str);
+            Console.WriteLine();
         }
     }
 }
---
+++ library_int.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib_int
+{
+    class library_int
+    {
+        public static int Min(int[] arr)
+        {
+            int min = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]<min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            return min;
+        }
+        public static int Max(int []arr)
+        {
+            int max = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]>max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            return max;
+        }
+        public static int Summ(int []arr)
+        {
+            int summ = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                summ += arr[i];
+            }
+
+            return summ / arr.Length;
+        }
+        public static void Revers(int []arr)
+        {
+            int []arr_2 = arr;
+            int count = arr.Length-1;
+            int num = 0;
+
+            for (int i = 0; i < arr.Length/2; i++)
+            {
+                num = arr[i];
+                arr[i] = arr[count];
+                arr[count] = num;
+                count = count - 1;
+            }
+        }
+        public static string Conver(int []arr)
+        {
+            StringBuilder str = new StringBuilder();
+
+            str.Append("[");
+            str.Append(string.Join(",", arr));
+            str.Append("]");
+
+            string result = str.ToString();
+            return result;
+        }
+    }
+}
